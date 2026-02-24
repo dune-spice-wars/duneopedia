fc6313dc3b81c4e17518270737be3f26bc00b2c7
Aryytel <32339033+Aryytel@users.noreply.github.com>
2026-02-24 17:49:29 +0200

    Faction buildings

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
 
+const isProd = process.env.NODE_ENV === 'production';
+
 export default defineConfig({
   site: 'https://dune-spice-wars.github.io',
-  base: '/duneopedia/',
+  base: isProd ? '/duneopedia/' : '/',
   output: 'static',
   build: {
     format: 'file',
